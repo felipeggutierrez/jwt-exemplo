a46b9845a9dc1eca07b7332f1a96aca1e8194baf
Felipe Gutierrez <felipe.gutierrez@pbh.gov.br>
2020-06-16 21:05:02 -0300

    Commit archetype

---
+++ src/main/java/com/example/jwtexemplo/JwtExemploApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtexemplo/JwtExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
